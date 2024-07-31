96727532514640e5163dbe6b442ed8c89e9fca32
Haleyou21 <107396439+Haleyou21@users.noreply.github.com>
2024-07-30 23:14:33 -0400

    Update condition_analysis.sql

--- condition_analysis.sql
+++ condition_analysis.sql
@@ -1,16 +1,21 @@
--- Identifying the most common conditions
+-- Identifying the most common conditions and their average duration
 SELECT
-  condition_concept_id,
+  co.condition_concept_id,
   c.concept_name AS condition_name,
-  COUNT(condition_occurrence_id) AS occurrence_count
+  COUNT(co.condition_occurrence_id) AS occurrence_count,
+  AVG(DATE_DIFF(co.condition_end_date, co.condition_start_date)) AS avg_duration_days,
+  COUNT(DISTINCT co.person_id) AS affected_patients,
+  (COUNT(co.condition_occurrence_id) / COUNT(DISTINCT co.person_id)) AS avg_conditions_per_patient
 FROM
   `bigquery-public-data.cms_synthetic_patient_data_omop.condition_occurrence` AS co
 JOIN
   `bigquery-public-data.cms_synthetic_patient_data_omop.concept` AS c
 ON
   co.condition_concept_id = c.concept_id
+WHERE
+  co.condition_end_date IS NOT NULL
 GROUP BY
-  condition_concept_id, condition_name
+  co.condition_concept_id, condition_name
 ORDER BY
   occurrence_count DESC
 LIMIT 100;
